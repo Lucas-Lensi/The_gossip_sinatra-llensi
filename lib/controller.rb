f9a923abbf9670b2f94313ee458b794903cfff18
llensi <lucas.lensi@gmail.com>
2019-10-21 17:26:04 +0200

    add commentaires

--- lib/controller.rb
+++ lib/controller.rb
@@ -18,7 +18,10 @@ class ApplicationController < Sinatra::Base
   get '/gossips/:id/' do
     id = params['id']
     to_print = Gossip.find(id)
-    erb :show, locals: {gossip: to_print, nb: id}
+    if to_print.comment != nil
+      comment = to_print.comment.split('..;..')
+    end
+    erb :show, locals: {gossip: to_print, nb: id, comment: comment}
   end
 
   get '/gossips/:id/edit/' do
@@ -30,6 +33,12 @@ class ApplicationController < Sinatra::Base
   post '/gossips/:id/edit/' do
     id = params['id'].to_i
     Gossip.edit(id, params["new_gossip_author"], params["new_gossip_content"])
+    redirect '/'
+  end
+
+  post '/gossips/:id/' do
+    id = params['id'].to_i
+    Gossip.comment(id, params["gossip_comment"])
     redirect '/'
   end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,23 +1,24 @@
 require 'csv'
 
 class Gossip
-  attr_accessor :author, :content
+  attr_accessor :author, :content, :comment
 
-  def initialize(author, content)
+  def initialize(author, content, comment='')
     @author = author
     @content = content
+    @comment = comment
   end
 
   def save
     CSV.open("./db/gossip.csv", "ab") do |csv|
-      csv << [@author, @content]
+      csv << [@author, @content, @comment]
     end
   end
 
   def self.all
     all_gossips = Array.new
     CSV.read("./db/gossip.csv").each do |csv_line|
-      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+      all_gossips << Gossip.new(csv_line[0], csv_line[1], csv_line[2])
     end
     return all_gossips
   end
@@ -31,6 +32,20 @@ class Gossip
     gossip_array = CSV.read("./db/gossip.csv")
     gossip_array[i-1][0] = new_author
     gossip_array[i-1][1] = new_content
+    CSV.open("./db/gossip.csv", "w") do |csv|
+      gossip_array.each do |potin|
+        csv << potin
+      end
+    end
+  end
+
+  def self.comment(i, new_comment)
+    gossip_array = CSV.read("./db/gossip.csv")
+    if gossip_array[i-1][2] == nil
+      gossip_array[i-1][2] = new_comment
+    else
+      gossip_array[i-1][2] = gossip_array[i-1][2] + '..;..' + new_comment
+    end
     CSV.open("./db/gossip.csv", "w") do |csv|
       gossip_array.each do |potin|
         csv << potin
